dd3b4b472cc6a810e81d1e0b819dbb6e0a815d8d
SOURAV SINHA <sourav.sinha@latentbridge.com>
2024-01-28 23:48:05 +0530

    in progress

--- web_development/base/models.py
+++ web_development/base/models.py
@@ -1,3 +1,14 @@
-from django.db import models
+from django.db.models import Model,  CharField, BigIntegerField, TextField
+
 
 # Create your models here.
+class JobModel(Model):
+    id = BigIntegerField(primary_key=True, )
+    title = CharField(max_length=100)
+    location = CharField(max_length=100)
+    description = TextField()
+
+    class Meta:
+        # Specify the table name and schema name
+        db_table = 'jobs'
+        # db_schema = 'public'
--- web_development/base/views.py
+++ web_development/base/views.py
@@ -1,9 +1,6 @@
 from django.shortcuts import render
 
-# Create your views here.
-from django.http import HttpResponse
-
 
 def home(request):
-    return HttpResponse('home page')
+    return render(request, 'home.html')
 
